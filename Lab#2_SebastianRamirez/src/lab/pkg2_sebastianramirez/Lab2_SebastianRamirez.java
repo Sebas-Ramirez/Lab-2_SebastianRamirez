7f421189ea54338094aa58c067d351daf071394e
Sebas-Ramirez <ramirez.sebaas@gmail.com>
2020-01-31 13:26:29 -0600

    Creacion de Atributos y contrusctores

---
+++ Lab#2_SebastianRamirez/src/lab/pkg2_sebastianramirez/Lab2_SebastianRamirez.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab.pkg2_sebastianramirez;
+
+/**
+ *
+ * @author cotip
+ */
+public class Lab2_SebastianRamirez {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ Lab#2_SebastianRamirez/src/lab/pkg2_sebastianramirez/Personajes.java
@@ -0,0 +1,25 @@
+package lab.pkg2_sebastianramirez;
+public class Personajes {
+    private String personaje;
+    private String raza;
+    private double estatura;
+    private double peso;
+    private int años;
+    private String descripcion;
+    private String nacionalidad;
+
+    public Personajes() {
+    }
+
+    public Personajes(String personaje, String raza, double estatura, double peso, int años, String descripcion, String nacionalidad) {
+        this.personaje = personaje;
+        this.raza = raza;
+        this.estatura = estatura;
+        this.peso = peso;
+        this.años = años;
+        this.descripcion = descripcion;
+        this.nacionalidad = nacionalidad;
+    }
+
+    
+}
